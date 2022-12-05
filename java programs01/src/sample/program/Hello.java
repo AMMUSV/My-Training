9e6269f2fe9d15259c758739a934052197554c75
HP <HP@DESKTOP-EOTA7AE>
2022-12-05 15:11:16 +0530

    my program

---
+++ java programs01/src/sample/program/Hello.java
@@ -0,0 +1,19 @@
+package sample.program;
+
+public class Hello {
+	
+	public void print()
+	{
+		System.out.println("HELLO WORLD");
+	}
+	
+	
+
+	public static void main(String[] args)
+	{
+		Hello obj = new Hello();
+		obj.print();
+
+	}
+
+}
